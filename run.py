4565d57247958296f20606655638f3ed52cefac1
johnamdickson <>
2023-05-13 13:25:09 +0000

    feat: add function to return row data per date entry

--- run.py
+++ run.py
@@ -13,5 +13,17 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('historical-weather-data')
 
 worksheet_test = SHEET.worksheet('archive')
-print(worksheet_test.row_values(1))
+# print(worksheet_test.row_values(1))
 
+def find_historical_data_row(date):
+    """
+    Function to find row in historical data spreadsheet and return data from row as list.
+    """
+    # Solution to selecting cell from Stack Overflow:
+    # https://stackoverflow.com/questions/65234180/how-to-find-a-row-based-on-an-id-and-then-edit-the-row-with-gspread-python
+    cell = worksheet_test.find(date,in_column = 1)
+    weather_data = worksheet_test.row_values(cell.row)
+    print (weather_data)
+    return weather_data
+
+find_historical_data_row('01/01/1942')
