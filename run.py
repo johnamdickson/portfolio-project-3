7347077edaf9d1fd7b755e1f58a6da1885f0627f
johnamdickson <>
2023-05-14 14:42:26 +0000

    feat: add function to calculate date range for user info

--- run.py
+++ run.py
@@ -15,8 +15,18 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('historical-weather-data')
 
-worksheet_test = SHEET.worksheet('archive')
-# print(worksheet_test.row_values(1))
+weather_archive_sheet = SHEET.worksheet('archive')
+
+
+def find_date_range():
+    """
+    Calculate date range in worksheet to display to user before making
+    their entry.
+    """
+    earliest_date = weather_archive_sheet.cell(2, 1).value
+    row_count = len(weather_archive_sheet.get_all_values())
+    latest_date = weather_archive_sheet.cell(row_count, 1).value
+    return [earliest_date, latest_date]
 
 def find_historical_data_row(date):
     """
@@ -26,17 +36,18 @@ def find_historical_data_row(date):
     # Solution to selecting cell from Stack Overflow:
     # https://stackoverflow.com/questions/65234180/how-to-find-a-row-based-on-an-id-and-then-edit-the-row-with-gspread-python
     print(f"Locating data for {date}")
-    cell = worksheet_test.find(date, in_column=1)
-    weather_data = worksheet_test.row_values(cell.row)
+    cell = weather_archive_sheet.find(date, in_column=1)
+    weather_data = weather_archive_sheet.row_values(cell.row)
     print(weather_data)
     return weather_data
 
-def get_date():
+def get_date(sheet_dates):
     """
     Request date from user to locate historical weather data.
     """
     while True:
-        print("Please enter the date to check the historical weather data.")
+
+        print(f"Please enter the date to check the historical weather data.\nAvailable dates between {sheet_dates[0]} and {sheet_dates[1]}.")
         print("The date format should be: DD/MM/YYYY")
         print("Example: 30/04/1978\n")
         date = input("Enter your date here:\n")
@@ -67,7 +78,8 @@ def validate_date(date):
         return False
 
 def main():
-    user_date = get_date()
+    available_dates = find_date_range()
+    user_date = get_date(available_dates)
     find_historical_data_row(user_date)
-
+    find_date_range()
 main()
