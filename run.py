a67298942288a560d1cc3938afa16292d9c5eec5
johnamdickson <>
2023-05-19 13:16:14 +0000

    test: wire up open weather api and test pyowm functionality

--- run.py
+++ run.py
@@ -4,10 +4,11 @@ import time
 from termcolor import colored, cprint
 import os
 import menus
+import weather_forecast as wf
 
 
 def main():
-    menus.main_menu()
-
+    # menus.main_menu()
+    wf.weather_forecast()
 
 main()
