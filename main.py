cc3cc23e7b967be458820f6fb5bbac649881fd12
johnamdickson <johnamdickson@me.com>
2023-06-02 07:37:14 +0000

    fix: add restart user selection post 3 day summary

--- main.py
+++ main.py
@@ -174,7 +174,8 @@ def run_weather_forecast():
         if user_option == 5:
             forecast.print_three_day_summary(day_one, day_two, day_three)
             input("Hit return to access other user options.")
-            user_options(4)
+            user_option = user_options(4)
+            restart_user_selection(user_option)
         else:
             restart_user_selection(user_option)
 
