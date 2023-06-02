e4023e70d5a520d57ddc334c1ecc7c86ffbb00c8
johnamdickson <johnamdickson@me.com>
2023-06-02 07:38:48 +0000

    fix:further adjust 3 day summary column width

--- classes.py
+++ classes.py
@@ -434,7 +434,7 @@ class ForecastWeather():
                  [day_two[0], day_two[2], day_two[3],day_two[5], day_two[4]],
                  [day_three[0], day_three[2], day_three[3], day_three[5], day_three[4]]
                  ]
-        print(tabulate(table, headers = ["Date", "Day Temp", "Night Temp","Conditions", "Wind"],tablefmt="rounded_grid", maxcolwidths=[8, 8, 8, 8,25]))
+        print(tabulate(table, headers = ["Date", "Day Temp", "Night Temp","Conditions", "Wind"],tablefmt="rounded_grid", maxcolwidths=[8, 7, 7, 7,25]))
 
 class LoadingScreens:
     """
