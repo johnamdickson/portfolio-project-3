554ccb6c801a43574ac7f4bf3faa69a2478b0de4
johnamdickson <johnamdickson@me.com>
2023-06-02 07:33:19 +0000

    fix: further amend table columns 3 day summary

--- classes.py
+++ classes.py
@@ -429,11 +429,12 @@ class ForecastWeather():
         input("\nHit return to see the next day's forcast\n")
 
     def print_three_day_summary(self, day_one, day_two, day_three):
+        os.system('clear')
         table = [[day_one[0], day_one[2], day_one[3],day_one[5], day_one[4]],
                  [day_two[0], day_two[2], day_two[3],day_two[5], day_two[4]],
                  [day_three[0], day_three[2], day_three[3], day_three[5], day_three[4]]
                  ]
-        print(tabulate(table, headers = ["Date", "Day Temp", "Night Temp","Conditions", "Wind"],tablefmt="rounded_grid", maxcolwidths=[10, 7, 7, 10,25]))
+        print(tabulate(table, headers = ["Date", "Day Temp", "Night Temp","Conditions", "Wind"],tablefmt="rounded_grid", maxcolwidths=[8, 8, 8, 8,25]))
 
 class LoadingScreens:
     """
