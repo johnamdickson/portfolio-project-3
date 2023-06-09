b16bc7baa28a27465b7674e5fd9d3f97abd8fb25
johnamdickson <johnamdickson@me.com>
2023-06-09 01:17:07 +0000

    refactor: error correction from linter feedback

--- functions.py
+++ functions.py
@@ -2,10 +2,11 @@ from time import sleep
 from os import system
 from termcolor import colored, cprint
 
+
 def print_error_message(string, delay):
     """
-    Function to print off error message with 
-    steps to clear screen, print in color and 
+    Function to print off error message with
+    steps to clear screen, print in color and
     delay prior to clearing again.
     """
     system('clear')
@@ -14,9 +15,10 @@ def print_error_message(string, delay):
     system('clear')
     return
 
+
 def format_data_strings(string, colour):
     """
-    Function to format data points that will be 
+    Function to format data points that will be
     highlighted blue for feedback to user.
     """
     return colored(string, colour, None, ['bold'])
